300a845964b788e009462f52315f680704b34b84
iqbal hassan <emiqball111@gmail.com>
2023-07-11 11:43:39 +0500

    UPDATE THE UI

--- src/components/StateAgent/Card/DashboardCard.jsx
+++ src/components/StateAgent/Card/DashboardCard.jsx
@@ -5,6 +5,482 @@ const DashboardCard = () => {
     <>
       <Container>
         <Row className="mt-5">
+          <Col lg={3} md={6} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Monitoring Plan <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>87</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">
+                    Approvel Request
+                  </h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={6} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Monitoring Plan <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>07</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Active</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={6} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Reports <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>87</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Pending OMC</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={6} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Reports <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>18</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Active</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Monitoring Plan <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>87</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">
+                    Approvel Request
+                  </h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Monitoring Plan <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>07</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Active</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Reports <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>87</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Pending OMC</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Reports <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>18</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Active</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Monitoring Plan <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>87</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">
+                    Approvel Request
+                  </h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Monitoring Plan <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>07</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Active</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Reports <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>87</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Pending OMC</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Reports <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>18</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Active</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 d-flex justify-content-between px-0"
+              >
+                Emission Monitoring Plan <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 p-1">
+                  <h1>87</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">
+                    Approvel Request
+                  </h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Monitoring Plan <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>07</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Active</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Reports <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>87</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Pending OMC</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Reports <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>18</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Active</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Monitoring Plan <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>87</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">
+                    Approvel Request
+                  </h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Monitoring Plan <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>07</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Active</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Reports <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>87</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Pending OMC</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Reports <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>18</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Active</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Monitoring Plan <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>87</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">
+                    Approvel Request
+                  </h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Monitoring Plan <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>07</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Active</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Reports <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>87</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Pending OMC</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Reports <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>18</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Active</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Monitoring Plan <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>87</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">
+                    Approvel Request
+                  </h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Monitoring Plan <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>07</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Active</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Reports <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>87</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Pending OMC</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col lg={3} md={4} sm={6}>
+            <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
+              <Card.Header
+                as={"h6"}
+                className="bg-transparent py-3 px-1 d-flex justify-content-between"
+              >
+                Emission Reports <a href="#">More</a>
+              </Card.Header>
+              <Card.Body>
+                <div className="d-flex align-item-center gap-1 px-1">
+                  <h1>18</h1>
+                  <h6 className="pt-3 text-secondary fw-normal">Active</h6>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-5">
           <Col lg={3} md={4} sm={6}>
             <Card className="border border-white shadow p-1 mb-1 bg-body rounded">
               <Card.Header
